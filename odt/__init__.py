03afc49ed78f8482bba9643bd91554be3467df6f
mosesbaraza <clonesoftwares@gmail.com>
2018-08-25 21:15:05 +0300

    added readodt to read text strings from .odt doc

--- odt/__init__.py
+++ odt/__init__.py
@@ -8,15 +8,15 @@ class ODTFile:
         self.libre_doc()
 
     def libre_doc(self):
+        import zipfile
+        import xml.etree.ElementTree as ET
         try:
             import os
             if not (os.path.exists(self.filename) and self.filename.endswith('.odt')):
                 raise 'File doesnot exists or not a .odt file'
-            import zipfile
             with zipfile.ZipFile(self.filename, 'r') as stream:
                 with stream.open('meta.xml', 'r') as k:
                     xml_string=k.read()
-                    import xml.etree.ElementTree as ET
                     tree=ET.fromstring(xml_string)
                     for child in tree:
                         if child.tag == '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}meta':
@@ -28,6 +28,23 @@ class ODTFile:
                                 elif sub_child.tag=='{urn:oasis:names:tc:opendocument:xmlns:meta:1.0}generator':
                                     self.document_generator = sub_child.text #document generator application and environment
                 k.close()
+
+                self.text_list=[]
+
+                with stream.open('content.xml', 'r') as content_stream:
+                    tree=ET.fromstring(content_stream.read())
+                    for child in tree:
+                        if child.tag=='{urn:oasis:names:tc:opendocument:xmlns:office:1.0}body': #body tag 
+                            for subchild in child:
+                                if subchild.tag=='{urn:oasis:names:tc:opendocument:xmlns:office:1.0}text':
+                                    for subsubchild in subchild:
+                                        if subsubchild.tag=='{urn:oasis:names:tc:opendocument:xmlns:text:1.0}p': #p tag (paragraph tag) containing strings of text
+                                            if subsubchild.text==None: #empty tags with no text are empty lines
+                                                self.text_list.append('\n') #for empty lines append a new line character
+                                            else:
+                                                self.text_list.append(subsubchild.text)
+                content_stream.close()
+                
             stream.close()
         except zipfile.BadZipFile:
             raise 'Read Fail: Bad .odt file'
@@ -40,5 +57,14 @@ class ODTFile:
 
     def documentgenerator(self):
         return self.document_generator
+
+    def readodt(self):
+        return self.text_list
+
+
+# path='/home/coder/Desktop/mydoc.odt'
+x=ODTFile('/home/coder/Desktop/mydoc.odt')
+print(x.readodt())
+print(x.creationdatetime())
 
 
